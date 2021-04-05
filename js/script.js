946be15942913aef6120644d1423e0406be76d4c
Gustavo <gustavoguedez.men@gmail.com>
2021-04-05 15:07:25 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,97 +10,86 @@ document.getElementById("n9").addEventListener("click",n9);
 document.getElementById("n0").addEventListener("click",n0);
 document.getElementById("s").addEventListener("click",o1);
 document.getElementById("r").addEventListener("click",o2);
-document.getElementById("m").addEventListener("click",o3);
-document.getElementById("d").addEventListener("click",o4);
-document.getElementById("sr").addEventListener("click",sr);
-document.getElementById("cl").addEventListener("click",cl);
-
+document.getElementById("d").addEventListener("click",o3);
+document.getElementById("m").addEventListener("click",o4);
+document.getElementById("sr").addEventListener("click",showResult);
 
 function n1() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n1').innerHTML;
-	document.getElementById('resultado').innerHTML = actual +sumado;
+	let sumado = document.getElementById("n1").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n2() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n2').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n2").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n3() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n3').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n3").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n4() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n4').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n4").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n5() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n5').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n5").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n6() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n6').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n6").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n7() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n7').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n7").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n8() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n8').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n8").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n9() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n9').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n9").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
-
 function n0() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('n0').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("n0").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
 
 function o1() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('s').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("s").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
 
 function o2() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('r').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("r").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
 
 function o3() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('m').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("d").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
 
 function o4() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById('d').innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado;
+	let sumado = document.getElementById("m").innerHTML;
+	document.getElementById('resultado').innerHTML = actual +  sumado
 }
 
-function sr() {
+function showResult() {
 	let actual = document.getElementById('resultado').innerHTML;
 	let suma = actual.indexOf("+");
 	let resta = actual.indexOf("-");
@@ -126,11 +115,4 @@ function sr() {
 		document.getElementById("resultado").innerHTML = res;
 		
 	}
-
 }
-
-function cl() {
-	document.getElementById('resultado').innerHTML = " ";
-}
-
-
